ca7e16d56c1f21d4ab096457f8b2b509b2cf2c7d
ShariffGoon <63655536+ShariffGoon@users.noreply.github.com>
2024-12-12 12:34:50 +0000

    Set up Spring Boot project structure and add application.properties

---
+++ src/main/java/com/example/Konain_FinalTest_COMP303/KonainFinalTestComp303Application.java
@@ -0,0 +1,13 @@
+package com.example.Konain_FinalTest_COMP303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonainFinalTestComp303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonainFinalTestComp303Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Konain_FinalTest_COMP303/KonainFinalTestComp303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Konain_FinalTest_COMP303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonainFinalTestComp303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
